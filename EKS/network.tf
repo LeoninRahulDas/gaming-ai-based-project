d0b4a46fe16bc7433fdf6935fc0e5f21dc018746
leo <leo@gmail.com>
2026-05-13 18:42:37 +0530

    modified network.tf

--- EKS/network.tf
+++ EKS/network.tf
@@ -18,7 +18,7 @@ resource "aws_subnet" "quantam_subnet" {
   vpc_id     = aws_vpc.quantam_vpc.id
   cidr_block = cidrsubnet(aws_vpc.quantam_vpc.cidr_block, 8, count.index)
 
-  availability_zone       = element(["ap-northeast-1a", "ap-northeast-1c"], count.index)
+  availability_zone       = element(["ap-southeast-1a", "ap-southeast-1c"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
